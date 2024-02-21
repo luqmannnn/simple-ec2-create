b606ceb6ff99f5cd8ef32781d4804921ce8355a4
iuolxvpq <luqmannurhakimt@gmail.com>
2024-02-21 19:37:29 +0800

    Create ec2

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "compute_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  subnet_id     = data.aws_subnet.selected_subnet.id
   associate_public_ip_address = true
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.my_sg.id]
